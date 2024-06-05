021e08ce623675c3ea64e230d18e62cc44af4549
yash <yashmartand@gmail.com>
2024-06-05 17:59:02 +0530

    mul is added in calc

--- src/main/java/mypkg/Calculator.java
+++ src/main/java/mypkg/Calculator.java
@@ -12,10 +12,16 @@ public class Calculator {
     {
         return(a-b);
     }
+    public int mul(int a, int b)
+    {
+        return(a*b);
+    }
     public static void main(String[] args)
     {
         Calculator ob = new Calculator();
         System.out.println("sum is "+ob.sum(20,10));
+        System.out.println("sub is "+ob.sub(20,10));
+        System.out.println("mul is "+ob.mul(20,10));
     }
 	
 
